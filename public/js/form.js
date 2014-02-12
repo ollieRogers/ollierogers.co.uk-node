66047eb365b1f0384d175347f51c7bcd9f3c1072
Ollie <ollie@3ev.com>
2014-02-12 15:19:30 +0000

    add some dumb js form validation

--- public/js/form.js
+++ public/js/form.js
@@ -12,41 +12,52 @@
         allFields = $( "#message, #email, #name" ),  
         $submitButton = $("#submit"),
         processingClass = 'processing',
+        valid = false,
         dataString = 'name=' + name
                    + '&email=' + email
                    + '&message=' + message;
     
-
     /* 
      * On Submit
      */ 
-    event.preventDefault();            // prevent standard form submittion    
-    $form.addClass( processingClass ); // change form class while waiting for post response
-    $submitButton[0].disabled = true;  // only allow one submition at a time by disabling the submit button
+
+    event.preventDefault();  // prevent standard form submittion 
+    
+    (name.length * email.length * message.length === 0 )? valid = false : valid = true; // Do some dumb validation
+
 
 
     /* 
      * Post the form 
      */ 
-    $.ajax( {  
-      type: "POST",  
-      url: "/contact",  
-      data: dataString,  
-      success: function() {  
-        $submitButton[0].disabled = false;    // enable submit button again
-        $form.removeClass( processingClass ); // change back the forms class after submittion
-        allFields.val( '' );                  // reset inputs 
-        $form.addClass('thanks')
-        $submitButton.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){ 
-          setTimeout(function(){
-            $form.removeClass('thanks');
-          }, 500)
-        });
-      },  
-      error: function(){
-        // TODO do something for Form Error
-      }
-    }); 
+    if( valid ){
+
+      $form.addClass( processingClass ); // change form class while waiting for post response
+      $submitButton[0].disabled = true;  // only allow one submition at a time by disabling the submit button
+
+      $.ajax( {  
+        type: "POST",  
+        url: "/contact",  
+        data: dataString,  
+        success: function() {  
+          $submitButton[0].disabled = false;    // enable submit button again
+          $form.removeClass( processingClass ); // change back the forms class after submittion
+          allFields.val( '' );                  // reset inputs 
+          $form.addClass('thanks')
+          $submitButton.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){ 
+            setTimeout(function(){
+              $form.removeClass('thanks');
+            }, 500)
+          });
+        },  
+        error: function(){
+          // TODO do something for Form Error
+        }
+      });  
+    } else {
+      alert('NOPE')
+    }
+
 
   });
 
