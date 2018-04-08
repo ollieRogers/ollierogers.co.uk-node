b1fcd2f104151f51ec3ccb988c1ba515d76013e0
Ollie Rogers <ollie.rogers@rationetwork.co.uk>
2018-04-08 09:28:16 +0100

    add offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Ollie Rogers',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-offline',
+  ],
 };
